f014312439f831f443156992a10e0891b450b70b
ircal <matanliber11@gmail.com>
2020-03-23 17:41:47 +0200

    test

---
+++ begin/first.py
@@ -0,0 +1,8 @@
+reward = 0
+while reward < 1000000:
+    reward += 60
+    print("baasa")
+    print("another day")
+    print(reward)
+
+print("retire")
---
+++ begin/second.py
@@ -0,0 +1,9 @@
+YOUNG_AGE = 16
+ADULT_AGE = 21
+age = 17
+if age < YOUNG_AGE:
+    print("Young")
+elif age < ADULT_AGE:
+    print("hello")
+else:
+    print("Adult")
