694f0585ad6536f97fe17445cb1fac3d15ec5e8a
ircal <matanliber11@gmail.com>
2020-03-23 20:27:24 +0200

    added some files

--- begin/first.py
+++ begin/first.py
@@ -2,7 +2,9 @@ reward = 0
 while reward < 1000000:
     reward += 60
     print("baasa")
-    print("another day")
+    for i in xrange(1000000 - reward):
+        print("another day")
     print(reward)
 
 print("retire")
+print("hello")
